5324982b3e0e4543082cfa145a25e1f5c0072c51
Matheus Carvalho <matheusfelipe_contato@hotmail.com>
2021-08-27 08:13:40 -0300

    feat: lista de caminhos da API

--- siteBlog/blog/views.py
+++ siteBlog/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+def blogOverview(request):
+    api_urls = {
+        'List Posts': 'posts'
+    } 
+    return Response(api_urls)
