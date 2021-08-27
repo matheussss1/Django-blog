9a476c3db4942e1869c87e80e53db0d1602e2914
matheussss1 <matheusfelipe_contato@hotmail.com>
2021-08-27 00:04:41 -0300

    feat: decorator admin criando um list_display

--- siteBlog/blog/admin.py
+++ siteBlog/blog/admin.py
@@ -3,4 +3,6 @@ from django.contrib import admin
 from .models import Post
 # Register your models here.
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("titulo", "slug", "autor", "criado", "atualizado")
