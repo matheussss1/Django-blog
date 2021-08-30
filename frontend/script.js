36114fb55719bcf45136f4e7e2d4c3df5f170c56
Matheus Carvalho <matheusfelipe_contato@hotmail.com>
2021-08-30 10:58:23 -0300

    feat: modal front-end

--- frontend/script.js
+++ frontend/script.js
@@ -9,22 +9,65 @@ const options = {
     ContentType: 'application/json',
 }
 
-const data = fetchAPI('http://localhost:8000/api/posts/', options)
+const open_modal_noticia = ({target}) => {
+    fetchAPI(`http://localhost:8000/post/${target.id}`, options)
+        .then(data => [data.body, data.titulo])
+}
+
+const open_modal_listener = () => {
+    const elements = Array.from(document.querySelectorAll("a[id^='post#']"))
+    elements.forEach(e => {
+        e.addEventListener('click', () => {
+            const path = e.id.replace('post#', '');
+            fetchAPI(`http://localhost:8000/post/${path}`, options)
+            .then(data => {
+                const overlay = document.getElementById('overlay');
+                const modal = document.getElementById('modal');
+                const modal_content = document.getElementById('modal__content');
+                const titulo = document.getElementById('modal__titulo');
+                overlay.style.display = 'block';
+                modal.style.display = 'block';
+                modal_content.innerHTML += data.body;
+                titulo.innerHTML = data.titulo; 
+            })
+        })
+    });
+}
+
+const close_modal_listener = () => {
+    document.addEventListener('click', ({target}) =>{
+        target.id === 'overlay' && (() => {
+            document.getElementById('overlay').style.display = 'none'
+            document.getElementById('modal').style.display = 'none'
+        })()
+    })
+}
+
+const add_listeners = () => {
+    open_modal_listener();
+    close_modal_listener();
+}
+
+const list_posts = () => {
+    fetchAPI('http://localhost:8000/posts/', options)
     .then(data => {
-        console.log(data)
         data.map(e => {
-            const status = e.status == 'R'? 'Rascunho' : 'Postado'
+            const status = e.status == 'R'? 'Rascunho' : 'Postado';
             document.querySelector('.post').innerHTML += 
-            `
-                <tr>
-                    <td><a href="${e.slug}">${e.titulo}</a></td>
+                `<tr>
+                    <td><a id=post#${e.slug}>${e.titulo}</a></td>
                     <td>${e.autor['first_name']}</td>
                     <td>${e.criado}</td>
                     <td>${e.atualizado}</td>
                     <td>${status}</td>
-                </tr>
-            `
+                </tr>`;
         })
+        add_listeners();
     });
+}
 
-    
+const init = () => {
+    list_posts();
+}
+
+init();
